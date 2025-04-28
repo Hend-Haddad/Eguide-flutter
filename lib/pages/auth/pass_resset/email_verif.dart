49dec744ebb6fb3b2186feb06a5d3ade08443081
HEND-HADDAD <hendhaddad2000@gmail.com>
2025-04-28 14:54:44 +0100

    pwd modifier

--- lib/pages/auth/pass_resset/email_verif.dart
+++ lib/pages/auth/pass_resset/email_verif.dart
@@ -1,16 +1,11 @@
 
-
-
-
-import 'package:eguideapp/utils/dims.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../utils/dims.dart';
 import '../../../utils/app_strings.dart';
 import '../../../utils/app_styles.dart';
-
-
 
 class EmailPage extends StatefulWidget {
   const EmailPage({super.key});
@@ -20,98 +15,109 @@ class EmailPage extends StatefulWidget {
 }
 
 class _EmailPageState extends State<EmailPage> {
-  final _emailController =TextEditingController();
+  final _emailController = TextEditingController();
 
   @override
   void dispose() {
-     _emailController.dispose();
+    _emailController.dispose();
     super.dispose();
   }
-  Future passwordReset() async{
-   try{
-     await FirebaseAuth.instance
-    .sendPasswordResetEmail(email: _emailController.text.trim());
-    showDialog(context: context, builder:(context){
-      return const AlertDialog(
-        content: Text('Password reset link sent! Check your email' ),
-      );
-    });
-   }on FirebaseAuthException catch(e){
+
+  Future passwordReset() async {
+  try {
+    await FirebaseAuth.instance
+        .sendPasswordResetEmail(email: _emailController.text.trim());
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          content: Text('Password reset link sent! Check your email'),
+        );
+      },
+    );
+  } on FirebaseAuthException catch (e) {
     print(e);
-    showDialog(context: context, builder:(context){
-      return AlertDialog(
-        content: Text(e.message.toString()),
-      );
-    });
-   }
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Text(e.message.toString()),
+        );
+      },
+    );
   }
+}
+
+    
+   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      
       body: SingleChildScrollView(
         child: SafeArea(
-           child: Container(
-            margin:AppDims.globalMargin,
+          child: Container(
+            margin: AppDims.globalMargin,
             child: Column(
-              
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        IconButton(onPressed: (){Navigator.pop(context);},
-                         icon: const Icon(CupertinoIcons.arrow_left))
-                      ],
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(CupertinoIcons.arrow_left),
+                    )
+                  ],
+                ),
+                Image.asset('assets/images/pass.jpg'),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      AppStrings.EmailPageForgetPass,
+                      style: AppStyles.authTitleStyle,
+                    )
+                  ],
+                ),
+                const SizedBox(height: 12),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Please enter the email associated with \n this account',
+                      style: TextStyle(color: Colors.black54, fontSize: 18),
                     ),
                   ],
                 ),
-               
-                Image.asset('assets/images/pass.jpg'),
-                const SizedBox(height: 30,), 
-                 Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                   children: [
-                   Text(AppStrings.EmailPageForgetPass,
-                   style: AppStyles.authTitleStyle,)
-      
-                      ],
-                  ),
-                  const SizedBox(height: 12,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      const Text(' Please enter the email associated with \n this account ',
-                      style: TextStyle(
-                        color: Colors.black54,
-                      fontSize: 18),),
-                    ],
-                  ),
-                  const SizedBox(height:20,),
-                  
-                  TextField(
-                    controller: _emailController ,
-                    decoration: InputDecoration(
-                    prefixIcon: const Icon(CupertinoIcons.at,size: 18,color: Colors.black54),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(CupertinoIcons.at, size: 18, color: Colors.black54),
                     hintText: 'Email',
                     border: OutlineInputBorder(
-                         borderRadius: BorderRadius.circular(20.0),
-                       ),
-                       ),
-                   ),
-                   const SizedBox(height:20,),
-                   Row(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
                   children: [
-                    Expanded(child: CupertinoButton(
-                      color: const Color.fromARGB(230, 94, 142, 206) ,
-                      child: const Text('Submit' ,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
-                       onPressed: (){
-
-                       }))
+                    Expanded(
+                      child: CupertinoButton(
+                        color: const Color.fromARGB(230, 94, 142, 206),
+                        child: const Text(
+                          'Submit',
+                          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        onPressed: () {
+                          passwordReset();
+                        },
+                      ),
+                    )
                   ],
                 ),
               ],
