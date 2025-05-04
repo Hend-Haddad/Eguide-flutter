aeb234f101ed25c6276053ed413cccd487912eec
HEND-HADDAD <hendhaddad2000@gmail.com>
2025-05-04 19:15:48 +0100

    modifer date de post avec format d'heure

--- lib/pages/auth/pass_resset/email_verif.dart
+++ lib/pages/auth/pass_resset/email_verif.dart
@@ -1,8 +1,6 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../../../utils/dims.dart';
 import '../../../utils/app_strings.dart';
 import '../../../utils/app_styles.dart';
@@ -16,40 +14,46 @@ class EmailPage extends StatefulWidget {
 
 class _EmailPageState extends State<EmailPage> {
   final _emailController = TextEditingController();
+  final FocusNode _emailFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _emailFocusNode.addListener(() => setState(() {}));
+  }
 
   @override
   void dispose() {
     _emailController.dispose();
+    _emailFocusNode.dispose();
     super.dispose();
   }
 
   Future passwordReset() async {
-  try {
-    await FirebaseAuth.instance
-        .sendPasswordResetEmail(email: _emailController.text.trim());
-    showDialog(
-      context: context,
-      builder: (context) {
-        return const AlertDialog(
-          content: Text('Password reset link sent! Check your email'),
-        );
-      },
-    );
-  } on FirebaseAuthException catch (e) {
-    print(e);
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          content: Text(e.message.toString()),
-        );
-      },
-    );
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: _emailController.text.trim());
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+            content: Text('Password reset link sent! Check your email'),
+          );
+        },
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(e.message.toString()),
+          );
+        },
+      );
+    }
   }
-}
 
-    
-   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -93,16 +97,43 @@ class _EmailPageState extends State<EmailPage> {
                   ],
                 ),
                 const SizedBox(height: 20),
-                TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(CupertinoIcons.at, size: 18, color: Colors.black54),
-                    hintText: 'Email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
+                
+                // Email Field with focus styling
+                Focus(
+                  onFocusChange: (_) => setState(() {}),
+                  child: TextField(
+                    controller: _emailController,
+                    focusNode: _emailFocusNode,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(vertical: 18, horizontal: 16),
+                      labelText: 'Email',
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      labelStyle: TextStyle(
+                        color: _emailFocusNode.hasFocus ? Colors.blue : Colors.black54,
+                      ),
+                      prefixIcon: Icon(
+                        CupertinoIcons.at,
+                        size: 18,
+                        color: _emailFocusNode.hasFocus ? Colors.blue : Colors.black54,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(
+                          color: _emailFocusNode.hasFocus ? Colors.blue : Colors.grey,
+                          width: 1.5,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                          width: 2.0,
+                        ),
+                      ),
                     ),
                   ),
                 ),
+                
                 const SizedBox(height: 20),
                 Row(
                   children: [
@@ -113,9 +144,7 @@ class _EmailPageState extends State<EmailPage> {
                           'Submit',
                           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
                         ),
-                        onPressed: () {
-                          passwordReset();
-                        },
+                        onPressed: passwordReset,
                       ),
                     )
                   ],
